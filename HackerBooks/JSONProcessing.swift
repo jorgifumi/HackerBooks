73d133e69dcd4d1c565c33c2f4255bec015e485f
Jorge Lucena <jorgifumi@gmail.com>
2015-12-06 17:46:25 +0100

    Cambio de NSMutableArray a Arrays, añado gitignore, termino primera versión de KCLibrary

---
+++ HackerBooks/JSONProcessing.swift
@@ -0,0 +1,9 @@
+//
+//  JSONProcessing.swift
+//  HackerBooks
+//
+//  Created by Jorge Miguel Lucena Pino on 6/12/15.
+//  Copyright © 2015 Jorge Miguel Lucena Pino. All rights reserved.
+//
+
+import Foundation
--- HackerBooks/KCLibrary.swift
+++ HackerBooks/KCLibrary.swift
@@ -12,11 +12,13 @@ class KCLibrary {
     
     //MARK: - Properties
     //Array de libros
-    var books   : NSMutableArray
+    var books   : [KCBook]
     
     //Array de tags con todas las distintas temáticas en orden alfabético. No puede haber repetidas
-    var tags    : NSMutableArray
+    var tags    : [String]
     
+    
+    //MARK: - Computed Variables
     //Número total de libros
     var booksCount  : Int{
         get{
@@ -26,8 +28,8 @@ class KCLibrary {
     }
     
     //MARK: - Init
-    init(books: NSMutableArray,
-        tags: NSMutableArray){
+    init(books: [KCBook],
+        tags: [String]){
             
             self.books = books
             self.tags = tags
@@ -36,16 +38,56 @@ class KCLibrary {
     //Número de libros de una temática concreta, si no existe devuelve 0
     func bookCountForTag (tag: String?) -> Int{
         
+        guard let tagValue = tag else{
+            return 0
+        }
+        
+        var num = 0
+        
+        for i in 0..<self.booksCount{
+            if self.books[i].tags.contains(tagValue) {
+                num++
+            }
+        }
+        
+        return num
     }
     
     //Array de los libros que hay en una temática. Un libro puede pertenecer a varias temáticas. Si no hay libros para una temática, devuelve nil
     func booksForTag (tag: String?) -> [KCBook]?{
         
+        guard let tagValue = tag else{
+            return nil
+        }
+        
+        if bookCountForTag(tagValue) == 0 {
+            return nil
+        }
+        
+        var books4Tag : [KCBook] = []
+        
+        for i in 0..<self.bookCountForTag(tagValue) {
+            if self.books[i].tags.contains(tagValue) {
+                books4Tag.append(self.books[i])
+            }
+        }
+        return books4Tag
     }
     
     //Un KCBook para el libro que está en la posición "index" de aquellos bajo un cierto tag. Si el índice o el tag no existe, devuelve nil
     
-    func bookAtIndex(index: Int) -> KCBook?{
+    func bookAtIndex(index: Int, tag: String?) -> KCBook?{
         
+        guard let tagValue = tag,
+        let books4Tag = booksForTag(tagValue) else{
+            return nil
+        }
+        
+        if books4Tag.count <= index {
+            return books4Tag[index]
+        }
+        
+        return nil
     }
+    
 }
